5edb5b7f0f4c34aa8e74dd808496a24557d8ad17
Manuel <manuel_8ale@hotmail.com>
2020-09-05 00:45:36 -0400

    added hook for swipe

---
+++ src/custom-hooks/useSwipeMobile.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react";
+import useEventListener from "./useEventListener";
+
+export default function useSwipeMobile(elementRef, callback) {
+  const [xStart, setXStart] = useState();
+  const [xEnd, setXEnd] = useState();
+  const [touchEnded, setEnded] = useState(false);
+  function getTouches(e) {
+    return e.touches || e.originalEvent.touches;
+  }
+
+  function handleTouchStart(e) {
+    const firstTouch = getTouches(e)[0];
+    console.log(firstTouch);
+    setXStart(firstTouch.clientX);
+  }
+
+  function handleTouchMove(e) {
+    const lastTouch = getTouches(e)[0];
+    console.log(lastTouch);
+    setXEnd(lastTouch.clientX);
+  }
+
+  function handleTouchEnd() {
+    setEnded(true);
+  }
+
+  useEventListener("touchstart", handleTouchStart, elementRef.current);
+  useEventListener("touchmove", handleTouchMove, elementRef.current);
+  useEventListener("touchend", handleTouchEnd, elementRef.current);
+
+  useEffect(() => {
+    console.log(elementRef);
+    if (touchEnded) {
+      if (!xStart || !xEnd) return;
+
+      let dx = xEnd - xStart;
+      let sign = Math.sign(dx);
+
+      if (sign === 1) callback[0](); //Swipe right
+      if (sign === -1) callback[1](); //Swipe left
+      setXStart(null);
+      setXEnd(null);
+      setEnded(false);
+    }
+  }, [xStart, xEnd, touchEnded]);
+}
